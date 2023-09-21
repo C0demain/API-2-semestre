b4cba7f690de66cbe6d154e58ec83885004c71c9
BrunoVieira003 <brunosilverio01@gmail.com>
2023-09-21 10:50:51 -0300

    Integra bot com tela do chat

--- bot/lib/src/main/java/GUI/TelaBot.java
+++ bot/lib/src/main/java/GUI/TelaBot.java
@@ -17,12 +17,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import bot.Bot;
 
 public class TelaBot extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField pergunta;
 
 	public void actionPerformed(ActionEvent e) {
 		TelaInicial.main(null);
@@ -44,9 +48,11 @@ public class TelaBot extends JFrame implements ActionListener{
 		lblNewLabel_2.setBounds(22, 29, 81, 14);
 		contentPane.add(lblNewLabel_2);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(22, 46, 310, 386);
-		contentPane.add(textArea);
+		JTextArea resposta = new JTextArea();
+		resposta.setLineWrap(true);
+		resposta.setEditable(false);
+		resposta.setBounds(22, 46, 310, 386);
+		contentPane.add(resposta);
 		
 		JLabel lblNewLabel_1 = new JLabel(" Pergunta :");
 		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 12));
@@ -54,16 +60,36 @@ public class TelaBot extends JFrame implements ActionListener{
 		lblNewLabel_1.setBounds(22, 443, 81, 14);
 		contentPane.add(lblNewLabel_1);
 		
-		textField = new JTextField();
-		textField.setBounds(22, 464, 204, 26);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		pergunta = new JTextField();
+		pergunta.setBounds(22, 464, 204, 26);
+		contentPane.add(pergunta);
+		pergunta.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Enviar");
-		btnNewButton.setForeground(new Color(0, 0, 0));
-		btnNewButton.setBackground(new Color(128, 128, 128));
-		btnNewButton.setBounds(236, 464, 96, 26);
-		contentPane.add(btnNewButton);
+		JButton buttomEnviar = new JButton("Enviar");
+		
+		// Interação com o bot (pegar pergunta e devolver resposta
+		buttomEnviar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				resposta.setText("Aguarde...");
+				try {
+					String resp = Bot.perguntar("FRTC.txt", pergunta.getText());
+					resposta.setText(resp);
+				}catch (RuntimeException err) {
+					resposta.setText("Erro na leitura de arquivo");
+					System.out.println(err);
+				}catch (Exception err2) {
+					resposta.setText("Desculpe, não consegui achar informações sobre '" + pergunta.getText() + "'");
+					System.out.println(err2);
+				}
+				
+			}
+		});
+		
+		buttomEnviar.setForeground(new Color(0, 0, 0));
+		buttomEnviar.setBackground(new Color(255, 255, 255));
+		buttomEnviar.setBounds(236, 464, 96, 26);
+		contentPane.add(buttomEnviar);
 		
 		JButton buttomVoltar = new JButton("Voltar");
 		buttomVoltar.setForeground(new Color(0, 0, 0));
---
+++ bot/lib/src/main/java/bot/Bot.java
@@ -0,0 +1,79 @@
+package bot;
+
+import dev.langchain4j.chain.ConversationalRetrievalChain;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.huggingface.HuggingFaceChatModel;
+import dev.langchain4j.model.huggingface.HuggingFaceEmbeddingModel;
+import dev.langchain4j.retriever.EmbeddingStoreRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static dev.langchain4j.data.document.FileSystemDocumentLoader.loadDocument;
+import static java.time.Duration.ofSeconds;
+
+
+
+
+public class Bot {
+	
+	//HuggingFace API
+    public static final String HF_API_KEY = "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";
+	
+	public static String perguntar(String arquivo, String pergunta) throws Exception {
+
+        Document document = loadDocument(toPath(arquivo));
+        
+        //choosing an embedding model
+        EmbeddingModel embeddingModel = HuggingFaceEmbeddingModel.builder()
+                .accessToken(HF_API_KEY)
+                .modelId("sentence-transformers/all-MiniLM-L6-v2")
+                .waitForModel(true)
+                .timeout(ofSeconds(90))
+                .build();
+        
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+
+        //embedding doc
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+        		.documentSplitter(DocumentSplitters.recursive(200))
+                .embeddingModel(embeddingModel)
+                .embeddingStore(embeddingStore)
+                .build();
+        ingestor.ingest(document);
+
+        
+        
+        
+ 
+        //choosing a model to predict
+        ConversationalRetrievalChain chain = ConversationalRetrievalChain.builder()
+                .chatLanguageModel(HuggingFaceChatModel.withAccessToken(HF_API_KEY))
+                .retriever(EmbeddingStoreRetriever.from(embeddingStore, embeddingModel))
+                // .chatMemory() // you can override default chat memory
+                // .promptTemplate() // you can override default prompt template
+                .build();
+        
+        
+        //predict
+        String answer = chain.execute(pergunta);
+        return answer;
+        
+	}
+	
+	private static Path toPath(String fileName) {
+        try {
+            URL fileUrl = Bot.class.getResource(fileName);
+            return Paths.get(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
