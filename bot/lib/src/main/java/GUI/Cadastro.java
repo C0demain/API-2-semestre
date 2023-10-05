c8270569353c82dc6e83c7a65a2c61aab8d357fe
Pedro Santos Kajiya <kajiyapedro@gmail.com>
2023-10-05 08:53:37 -0300

    add tela cadastro

---
+++ bot/lib/src/main/java/GUI/Cadastro.java
@@ -0,0 +1,116 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Toolkit;
+import javax.swing.JTextArea;
+import java.awt.FlowLayout;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import java.awt.Font;
+
+public class Cadastro extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Cadastro frame = new Cadastro();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Cadastro() {
+		setTitle("Login");
+		setForeground(new Color(128, 128, 128));
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 429, 446);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(53, 135, 241, 23);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton Entrar = new JButton("Cadastrar-se");
+		Entrar.setFont(new Font("Arial", Font.PLAIN, 11));
+		Entrar.setBackground(Color.LIGHT_GRAY);
+		Entrar.setForeground(Color.BLACK);
+		Entrar.setBounds(53, 343, 119, 23);
+		contentPane.add(Entrar);
+		
+		textField = new JTextField();
+		textField.setBounds(53, 59, 241, 23);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nome :");
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_1.setBounds(53, 34, 89, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Usuário :");
+		lblNewLabel_2.setForeground(Color.WHITE);
+		lblNewLabel_2.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_2.setBounds(53, 110, 75, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Senha :");
+		lblNewLabel_2_1.setForeground(Color.WHITE);
+		lblNewLabel_2_1.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_2_1.setBounds(53, 255, 46, 14);
+		contentPane.add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Email :");
+		lblNewLabel_1_1.setForeground(Color.WHITE);
+		lblNewLabel_1_1.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_1_1.setBounds(53, 179, 89, 14);
+		contentPane.add(lblNewLabel_1_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(53, 210, 241, 23);
+		contentPane.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(53, 280, 241, 23);
+		contentPane.add(textField_3);
+		
+		JLabel lblNewLabel = new JLabel("Entrar");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 10));
+		lblNewLabel.setBackground(Color.GRAY);
+		lblNewLabel.setIcon(new ImageIcon(Cadastro.class.getResource("/GUI/Design sem nome (1).png")));
+		lblNewLabel.setBounds(0, 0, 423, 414);
+		contentPane.add(lblNewLabel);
+	}
+}
