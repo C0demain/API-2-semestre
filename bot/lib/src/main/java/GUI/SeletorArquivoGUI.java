615fd0a0ded879c9f80f9b05a1e00944fe192dd6
Abner <abner.rmachado@gmail.com>
2023-10-05 09:58:19 -0300

    altera SeletorArquivoGUI e LimpaArquivo

--- bot/lib/src/main/java/GUI/SeletorArquivoGUI.java
+++ bot/lib/src/main/java/GUI/SeletorArquivoGUI.java
@@ -13,17 +13,25 @@ public class SeletorArquivoGUI extends JFrame implements ActionListener{
 	 */
 	private JPanel MainPanel;
     private JButton button1 = new JButton("Carregar");
+    private JButton button2 = new JButton("Chat");
     private JLabel nomeArquivo = new JLabel("Escolha seu arquivo", SwingConstants.CENTER);
     
     public static String caminhoArquivo;
-    private final JButton voltarButton = new JButton("Voltar");
 
     public SeletorArquivoGUI(){
+
+        button2.addActionListener(new ActionListener() {
+    	    @Override
+    	    public void actionPerformed(ActionEvent e) {
+    	        TelaBot telaBot = new TelaBot();
+    	        TelaBot.caminhoArquivo = caminhoArquivo;
+    	        telaBot.setVisible(true);
+    	        dispose(); // Fecha a janela atual (SeletorArquivoGUI)
+    	    }
+    	});
+
     	JPanel MainPanel = new JPanel(new BorderLayout());
 		MainPanel.add(nomeArquivo,BorderLayout.CENTER);
-		
-		voltarButton.addActionListener(this);
-		MainPanel.add(voltarButton, BorderLayout.NORTH);
 		MainPanel.add(button1,BorderLayout.SOUTH);
         
         setContentPane(MainPanel);
@@ -38,14 +46,16 @@ public class SeletorArquivoGUI extends JFrame implements ActionListener{
                 nomeArquivo.setText("Escolhendo arquivo");
                 JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
                 int r = fileChooser.showOpenDialog(null);
-                if (r == JFileChooser.APPROVE_OPTION)
-                {
-                	caminhoArquivo = fileChooser.getSelectedFile().getAbsolutePath();
+                if (r == JFileChooser.APPROVE_OPTION) {
+                    caminhoArquivo = fileChooser.getSelectedFile().getAbsolutePath();
                     nomeArquivo.setText(caminhoArquivo);
-                }
-                else
+                    MainPanel.remove(button1); // Remover o botão "Carregar"
+                    MainPanel.add(button2, BorderLayout.SOUTH); // Adicionar o botão "Chat"
+                    LimpaArquivo.limparArquivo(caminhoArquivo); // Limpa arquivo carregado pelo usuário
+                    
+                } else {
                     nomeArquivo.setText("Operação cancelada");
-                
+                }
             }
         });
     }
--- bot/lib/src/main/java/utilitarios/LimpaArquivo.java
+++ bot/lib/src/main/java/utilitarios/LimpaArquivo.java
@@ -6,56 +6,51 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.DosFileAttributeView;
+import java.nio.file.attribute.DosFileAttributes;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.FileAttributeView;
+
+
 
 public class LimpaArquivo {
 
-	public static void main(String[] args) {
-    	String arq = "C:\\Users\\Manhã\\Desktop\\prince.txt";
-    	String dir = "C:\\Users\\Manhã\\Desktop\\aux.txt";
+    public static void limparArquivo(String caminhoArquivo) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(arq , Charset.forName("UTF8")));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(dir , Charset.forName("UTF8")));
-            String line;	            
-            StringBuilder content = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new FileReader(caminhoArquivo, Charset.forName("UTF8")));
+            String dir = caminhoArquivo.replace(".txt", "_limpo.txt"); // Nome do arquivo limpo
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(dir, Charset.forName("UTF8")));
+            String line;
+
             while ((line = reader.readLine()) != null) {
                 line = line.trim();
                 if (!line.matches("\\d+")) {
-                    content.append(line).append("\n");
+                    line = line.replace(".", ".\n").replace("\n\n", "\n").replace("\t", "");
+                    writer.write(line + "\n");
                 }
             }
+
             reader.close();
-            content = new StringBuilder(content.toString().replace(".", ".\n"));
-            content = new StringBuilder(content.toString().replace("\n\n", "\n"));
-            content = new StringBuilder(content.toString().replace("\t", ""));
-            writer.write(content.toString());
             writer.close();
+            
+            // Tornar o arquivo oculto
+            Path arquivoPath = Path.of(dir);
+            Files.setAttribute(arquivoPath, "dos:hidden", true);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        arq = dir;
-        dir  = "C:\\Users\\Manhã\\Desktop\\result.txt";
-        
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(arq , Charset.forName("UTF8")));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(dir , Charset.forName("UTF8")));
-            String line;	            
-            StringBuilder content = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                line = line.trim();
-                if (!line.matches("\\d+")) {
-                    content.append(line).append("\n");
-                }
-            }
-            reader.close();
-            content = new StringBuilder(content.toString().replace(".", ".\n"));
-            content = new StringBuilder(content.toString().replace("\n\n", "\n"));
-            content = new StringBuilder(content.toString().replace("\t", ""));
-            writer.write(content.toString());
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Uso correto: java LimpaArquivo <caminho do arquivo>");
+            return;
         }
-        	        
+
+        String caminhoArquivo = args[0];
+        limparArquivo(caminhoArquivo);
     }
 }
