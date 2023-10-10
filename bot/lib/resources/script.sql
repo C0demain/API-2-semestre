889aa812b2c5e177a73ccc0567e382cce35b6776
Abner <abner.rmachado@gmail.com>
2023-10-10 19:56:10 -0300

    Update script.sql

--- bot/lib/resources/script.sql
+++ bot/lib/resources/script.sql
@@ -3,9 +3,15 @@ create database chatbot;
 use chatbot;
 
 create table usuarios
-(id int primary key auto_increment not null,
+(id int primary key auto_increment,
 nome varchar(255) not null,
-usuario varchar(255) not null unique,
-cpf varchar(13) not null unique,
+usuario varchar(50) not null unique,
+cpf varchar(14) not null unique,
 senha varchar(255) not null);
 
+create table registro_usuario
+(id int primary key auto_increment,
+id_usuario INT NOT NULL,
+data_hora_acesso DATETIME NOT NULL,
+descricao text,
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id) );
